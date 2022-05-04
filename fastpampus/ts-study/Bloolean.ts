b2e065059eec1d363ad7a875117bb1267262134a
leaisrevolution <leaisrevolution@gmail.com>
2022-05-05 00:10:34 +0900

    Boolean

---
+++ fastpampus/ts-study/Bloolean.ts
@@ -0,0 +1,9 @@
+//Bloolean
+
+let isDone: bloolean = false;
+
+isDone = true;
+console.log(typeof isDone); // 'boolean'
+
+let isOk: Boolean = true; // 일반적인 방식
+let isNotOk: boolean = new Boolean(true);
---
+++ fastpampus/tsc-project/test.js
@@ -0,0 +1,6 @@
+"use strict";
+let a;
+// a = "mark";
+a = 39;
+function hello(b) { }
+hello(39);
