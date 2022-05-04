ffbd893a0d7358e126421be42bcb611b2954b25d
leaisrevolution <leaisrevolution@gmail.com>
2022-05-05 00:19:25 +0900

    create my-books

--- fastpampus/tsc-project/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let a;
-// a = "mark";
-a = 39;
-function hello(b) { }
-hello(39);
--- fastpampus/tsc-project/test.ts
+++
@@ -1,7 +0,0 @@
-let a: number;
-// a = "mark";
-a = 39;
-
-function hello(b: number) {}
-
-hello(39);
