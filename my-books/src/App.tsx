d8d1368bbb54096cf8bec5fa22d7bc19f487cafb
leaisrevolution <leaisrevolution@gmail.com>
2022-05-05 00:39:46 +0900

    라우팅 설정하기

--- my-books/src/App.tsx
+++ my-books/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import Edit from "./pages/Edit";
+import Detail from "./pages/Detail";
+import Signin from "./pages/Singin";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/edit/:id" component={Edit} />
+        <Route exact path="/book/:id" component={Detail} />
+        <Route exact path="/add" component={Add} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
