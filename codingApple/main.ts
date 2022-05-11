dcc74e571baa436a83f75e3223274c44c77b288b
leaisrevolution <leaisrevolution@gmail.com>
2022-05-12 03:05:39 +0900

    HTML narrowing 방법

--- codingApple/main.ts
+++ codingApple/main.ts
@@ -1,1 +1,23 @@
-document.querySelector("#title");
+let 제목 = document.querySelector("#title");
+if (제목 != null) {
+  제목.innerHTML = "반가워요";
+}
+
+// Html narrowing 방법2 instanceof (가장 많이 사용)
+
+let 제목2 = document.querySelector("#title");
+if (제목2 instanceof Element) {
+  제목2.innerHTML = "반가워요";
+}
+
+// 방법3 as Element
+
+let 제목3 = document.querySelector("#title") as Element;
+제목3.innerHTML = "반가워요";
+
+// 방법4 오브젝트에 붙이는 ?.
+
+let 제목4 = document.querySelector("#title");
+if (제목4?.innerHTML) {
+  제목4.innerHTML = "반가워요";
+}
