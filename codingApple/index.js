a9d0dd2d732da5950859787bd9511a12816e8f50
leaisrevolution <leaisrevolution@gmail.com>
2022-05-12 01:10:42 +0900

    TS type

--- codingApple/index.js
+++ codingApple/index.js
@@ -1,2 +1,15 @@
 "use strict";
+// 무조건 string 타입만 들어올 수 있음
 let 이름 = "kim";
+let 나이 = 50;
+let 결혼했니 = null;
+let 결혼했니2 = undefined;
+//array 자료형
+let 회원들 = ["kim", "park"];
+let 회원들2 = [1, 2];
+// 변수 하나에 여러자료를 집어 넣고 싶을 땐, object 자료형으로 사용가능
+let 회원들3 = { member1: "kim", member2: "park" };
+let 회원들4 = {
+    member1: "kim",
+    member2: "park",
+};
--- codingApple/index.ts
+++ codingApple/index.ts
@@ -1,2 +1,18 @@
 // 무조건 string 타입만 들어올 수 있음
 let 이름: string = "kim";
+let 나이: number = 50;
+let 결혼했니: null = null;
+let 결혼했니2: undefined = undefined;
+
+//array 자료형
+let 회원들: string[] = ["kim", "park"];
+let 회원들2: number[] = [1, 2];
+
+// 변수 하나에 여러자료를 집어 넣고 싶을 땐, object 자료형으로 사용가능
+let 회원들3 = { member1: "kim", member2: "park" };
+
+// 타입 제한
+let 회원들4: { member1: string; member2: string } = {
+  member1: "kim",
+  member2: "park",
+};
