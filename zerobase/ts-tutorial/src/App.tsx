5f8b61df1f0d020376ccde65135a2aeeed52ea93
leaisrevolution <leaisrevolution@gmail.com>
2022-05-12 16:09:44 +0900

    hello react

--- zerobase/ts-tutorial/src/App.tsx
+++ zerobase/ts-tutorial/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Greetings from "./Greetings";
 
-function App() {
-  return <Greetings name="리액트" optional="optional" />;
-}
+const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(name);
+  };
+  return <Greetings name="리액트" onClick={onClick} />;
+};
 
 export default App;
