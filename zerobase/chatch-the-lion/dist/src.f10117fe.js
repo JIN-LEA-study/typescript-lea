edd2e236fb27eae88c285b1f24226d41ded9bf45
leaisrevolution <leaisrevolution@gmail.com>
2022-05-12 21:28:37 +0900

    프로젝트 구성 및 보드판 작성

--- zerobase/chatch-the-lion/dist/src.f10117fe.js
+++ zerobase/chatch-the-lion/dist/src.f10117fe.js
@@ -117,7 +117,86 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/Game.ts":[function(require,module,exports) {
+})({"src/Piece.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+},{}],"src/Board.ts":[function(require,module,exports) {
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Board = exports.Cell = void 0;
+
+var Piece_1 = require("./Piece");
+
+var Cell = /*#__PURE__*/function () {
+  function Cell(position, piece) {
+    _classCallCheck(this, Cell);
+
+    this.position = position;
+    this.piece = piece;
+    this.isActive = false;
+    this._el = document.createElement("DIV");
+
+    this._el.classList.add("cell");
+  }
+
+  _createClass(Cell, [{
+    key: "put",
+    value: function put(piece) {
+      this.piece = Piece_1.Piece;
+    }
+  }, {
+    key: "getPiece",
+    value: function getPiece() {
+      return this.piece;
+    }
+  }, {
+    key: "active",
+    value: function active() {
+      this.isActive = true;
+    }
+  }, {
+    key: "deactive",
+    value: function deactive() {
+      this.isActive = false;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.isActive) {
+        this._el.classList.add("active");
+      } else {
+        this._el.classList.remove("active");
+      }
+
+      this._el.innerHTML = this.piece ? this.piece.render() : "";
+    }
+  }]);
+
+  return Cell;
+}();
+
+exports.Cell = Cell;
+
+var Board = /*#__PURE__*/_createClass(function Board() {
+  _classCallCheck(this, Board);
+
+  this.cells = [];
+});
+
+exports.Board = Board;
+},{"./Piece":"src/Piece.ts"}],"src/Game.ts":[function(require,module,exports) {
 "use strict";
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -131,12 +210,16 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Game = void 0;
 
+var Board_1 = require("./Board");
+
 var Game = /*#__PURE__*/_createClass(function Game() {
   _classCallCheck(this, Game);
+
+  this.board = new Board_1.Board();
 });
 
 exports.Game = Game;
-},{}],"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+},{"./Board":"src/Board.ts"}],"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
