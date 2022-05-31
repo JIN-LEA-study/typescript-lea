22dc77d4862beabf53292b8a2dd72ac48be56638
leaisrevolution <leaisrevolution@gmail.com>
2022-05-31 23:18:25 +0900

    UI styles

--- projects/my-books/src/components/Signin.tsx
+++ projects/my-books/src/components/Signin.tsx
@@ -1,39 +1,52 @@
 import { Row, Col, Input, Button } from "antd";
+import styles from "./Signin.module.css";
 
 export default function Signin() {
   return (
-    <Row align="middle">
+    <Row align="middle" className={styles.signin_row}>
       <Col span={24}>
-        <Row>
-          <Col span={12}></Col>
+        <Row className={styles.signin_contents}>
           <Col span={12}>
-            <div>My books</div>
-            <div>Please Note Your Opinion</div>
-            <div />
-            <div>
-              Email
-              <span> *</span>
+            <img
+              src="/bg_signin.png"
+              className={styles.signin_bg}
+              alt="Signin"
+            />
+          </Col>
+          <Col span={12}>
+            <div className={styles.signin_title}>My books</div>
+            <div className={styles.signin_subtitle}>
+              Please Note Your Opinion
             </div>
-            <div>
+            <div className={styles.signin_underline} />
+            <div className={styles.email_title}>
+              Email
+              <span className={styles.required}> *</span>
+            </div>
+            <div className={styles.input_area}>
               <Input
                 placeholder="Email"
                 autoComplete="email"
                 name="email"
+                className={styles.input}
               ></Input>
             </div>
-            <div>
+            <div className={styles.password_title}>
               Password
-              <span> *</span>
+              <span className={styles.required}> *</span>
             </div>
-            <div>
+            <div className={styles.input_area}>
               <Input
                 type="password"
                 autoComplete="current-password"
                 name="email"
+                className="styles.input"
               ></Input>
             </div>
-            <div>
-              <Button size="large">Sign In</Button>
+            <div className={styles.button_area}>
+              <Button size="large" className={styles.button}>
+                Sign In
+              </Button>
             </div>
           </Col>
         </Row>
