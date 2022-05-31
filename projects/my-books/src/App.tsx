8a0e8840802db2959b3f75367a18d4d8f4d9e8c1
leaisrevolution <leaisrevolution@gmail.com>
2022-05-31 20:49:02 +0900

    add Error page

--- projects/my-books/src/App.tsx
+++ projects/my-books/src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Routes } from "react-router-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 
+// ErrorBoundary
+import { ErrorBoundary } from "react-error-boundary";
+
 // router page
 import Home from "./pages/Home";
 import Add from "./pages/Add";
@@ -9,19 +12,22 @@ import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
 import Signin from "./pages/Signin";
 import NotFound from "./pages/NotFound";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/edit/:id" element={<Edit />}></Route>
-        <Route path="/book/:id" element={<Detail />}></Route>
-        <Route path="/add" element={<Add />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/edit/:id" element={<Edit />}></Route>
+          <Route path="/book/:id" element={<Detail />}></Route>
+          <Route path="/add" element={<Add />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
