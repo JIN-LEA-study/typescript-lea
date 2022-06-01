015d1a436d53a9ce969dd26c665684fe13002c9f
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 18:15:33 +0900

    edit Signin page

--- projects/my-books/src/pages/Signin.tsx
+++ projects/my-books/src/pages/Signin.tsx
@@ -4,13 +4,12 @@ import { Redirect } from "react-router-dom";
 import SigninContainer from "../containers/SigninContainer";
 import { RootState } from "../types";
 
-export default function Signin() {
-  const token = useSelector<RootState, string | null>(
-    (state) => state.auth.token
-  );
-
+const SigninPage = () => {
+  const token = useToken();
   if (token !== null) {
     return <Redirect to="/" />;
   }
   return <SigninContainer />;
-}
+};
+
+export default SigninPage;
