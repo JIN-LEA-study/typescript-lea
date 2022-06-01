fdb296623bba2807d0d3c3391b0e0fc911cccd30
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 17:37:05 +0900

    edit UserService

--- projects/my-books/src/services/UserService.ts
+++ projects/my-books/src/services/UserService.ts
@@ -1,15 +1,26 @@
-import { LoginReqType } from "./../types";
 import axios from "axios";
 
+import { LoginReqType, LoginResType } from "../types";
+
 const USER_API_URL = "https://api.marktube.tv/v1/me";
+
 export default class UserService {
-  public static async login(reqData: LoginReqType) {
-    const response = await axios.post(USER_API_URL, reqData);
+  public static async login({
+    email,
+    password,
+  }: LoginReqType): Promise<string> {
+    const response = await axios.post<LoginResType>(USER_API_URL, {
+      email,
+      password,
+    });
     return response.data.token;
   }
+
   public static async logout(token: string): Promise<void> {
     await axios.delete(USER_API_URL, {
-      headers: { Authorization: `Bearer ${token}` },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
   }
 }
