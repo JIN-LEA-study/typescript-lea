91d19596d637cb61aae92a14e7042f0595c3e05f
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 17:14:28 +0900

    edit Home

--- projects/my-books/src/pages/Home.tsx
+++ projects/my-books/src/pages/Home.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { RootState } from "../types";
 
 export default function Home() {
-  return <div>hello</div>;
+  const dispatch = useDispatch();
+  const token = useSelector<RootState, string | null>(
+    (state) => state.auth.token
+  );
+
+  if (token === null) {
+    return <Redirect to="/signin" />;
+  }
+  return (
+  <div>
+    <div>Home</div>
+    <button onClick={click}>logout</button>;
+  </div>;
+  );
+
+  function click() {
+    dispatch(logout());
+  }
 }
