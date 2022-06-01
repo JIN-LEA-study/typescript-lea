12618e25db9e16dd57809a81cb6f2a97a3f5a804
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 18:14:50 +0900

    edit notFound page

--- projects/my-books/src/pages/NotFound.tsx
+++ projects/my-books/src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function NotFound() {
-  return <div>NotFound</div>;
-}
+const NotFound: React.FC = () => (
+  <div>
+    <h1>NotFound</h1>
+  </div>
+);
+
+export default NotFound;
