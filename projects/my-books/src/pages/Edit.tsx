b27073c441602c6d7c11f9b43996b34168c74a8b
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 18:13:41 +0900

    edit Edit page

--- projects/my-books/src/pages/Edit.tsx
+++ projects/my-books/src/pages/Edit.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
-export default function Edit() {
-  return <div>hello</div>;
-}
+import useToken from "../hooks/useToken";
+import EditContainer from "../containers/EditContainer";
+
+const Edit = () => {
+  const token = useToken();
+  if (token === null) {
+    return <Redirect to="/signin" />;
+  }
+  return <EditContainer />;
+};
+
+export default Edit;
