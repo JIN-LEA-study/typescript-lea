9df378255a01512aa2c16843423e87616904a6d0
lea <leaisrevolution@gmail.com>
2022-06-29 14:44:09 +0900

    [add] firebase_admin

---
+++ projects/blahx2-start/models/firebase_admin.ts
@@ -0,0 +1,24 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstance(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      // 초기화
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      // TODO 환경을 초기화 한다
+    }
+    return FirebaseAdmin.instance;
+  }
+}
