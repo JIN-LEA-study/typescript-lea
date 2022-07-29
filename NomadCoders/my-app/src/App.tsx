8330e18e88d8e409bfd78ce60a84500302a29e5b
leaisrevolution <leaisrevolution@gmail.com>
2022-07-29 14:13:21 +0900

    [add] Forms

--- NomadCoders/my-app/src/App.tsx
+++ NomadCoders/my-app/src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from "./Circle";
+import { useState } from "react";
+import { setConstantValue } from "typescript";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="lea" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onChange}
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
