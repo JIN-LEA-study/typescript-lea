645b0c89d1b527b4a4d3b2c37a1f1ff817521e75
leaisrevolution <leaisrevolution@gmail.com>
2022-07-29 16:38:14 +0900

    [add] useParams, Router, Setup

--- NomadCoders/my-app/src/App.tsx
+++ NomadCoders/my-app/src/App.tsx
@@ -1,22 +1,8 @@
-import { useState } from "react";
+import Router from "./routes/Router";
 import styled from "styled-components";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
 function App() {
-  return (
-    <div>
-      <Container>
-        <H1>Hello</H1>
-      </Container>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ NomadCoders/my-app/src/routes/Router.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coins from "./Coins";
+import Coin from "./Coin";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
