6b91492067ed736c561bf1924c1b9fcff0ce693c
leaisrevolution <leaisrevolution@gmail.com>
2022-07-29 13:50:29 +0900

    [add] Optional props

--- NomadCoders/my-app/src/Circle.tsx
+++ NomadCoders/my-app/src/Circle.tsx
@@ -1,9 +1,9 @@
-import { platform } from "os";
 import React from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -11,14 +11,22 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  // borderColor가 ?? 뒤에 있는 코드 실행
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? "white"}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
