d6960a7942962348b148445fde84438ec952b6c4
leaisrevolution <leaisrevolution@gmail.com>
2022-08-04 13:42:55 +0900

    [add] react version down

--- NomadCoders/my-app/src/index.tsx
+++ NomadCoders/my-app/src/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
+
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
