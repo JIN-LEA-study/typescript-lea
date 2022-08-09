f836c4b8a3acd0714644654dfead702d8ac7973c
leaisrevolution <leaisrevolution@gmail.com>
2022-08-09 23:30:32 +0900

    [add] Price Chart part Two

--- NomadCoders/my-app/src/routes/Chart.tsx
+++ NomadCoders/my-app/src/routes/Chart.tsx
@@ -2,15 +2,65 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "./api";
-
+import ApexChart from "react-apexcharts";
+interface IHistorixal {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 interface ChartProps {
   coinId: string;
 }
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorixal[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <div>Chart</div>;
+  console.log(data?.map((price) => price.close));
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              data: data?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
