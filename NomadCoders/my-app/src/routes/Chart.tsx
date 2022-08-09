2e4f3c1808b2f50c843684039e072834f8d63a37
leaisrevolution <leaisrevolution@gmail.com>
2022-08-09 22:19:50 +0900

    [add] Price Chart

--- NomadCoders/my-app/src/routes/Chart.tsx
+++ NomadCoders/my-app/src/routes/Chart.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "./api";
 
-function Chart() {
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return <div>Chart</div>;
 }
 
