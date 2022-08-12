664f467e9a15d935ddfeb9b9015532532f12b239
leaisrevolution <leaisrevolution@gmail.com>
2022-08-12 22:38:48 +0900

    [Add] Recoil porject start

--- Inflearn/learn-typescript/src/index.tsx
+++ Inflearn/learn-typescript/src/index.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>
 );
 
--- projects/recoil-todolist/src/App.tsx
+++ projects/recoil-todolist/src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import axios from "axios";
-import { RecoilRoot } from "recoil";
-import Calendar from "./components/Calendar";
 
 function App() {
-  return (
-    <RecoilRoot>
-      <Calendar />
-    </RecoilRoot>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
