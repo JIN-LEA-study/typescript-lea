b1ac5d5eb726bd2a4482ad746bd6583b8f92e694
leaisrevolution <leaisrevolution@gmail.com>
2022-08-15 22:23:53 +0900

    [add] counter reducer

---
+++ zerobase/todo-list/src/modules/counter.ts
@@ -0,0 +1,42 @@
+const INCREASE = "counter/INCREASE" as const;
+const DECREASE = "counter/DECREASE" as const;
+const INCREASE_BY = "counter/INCREASE_BY " as const;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+type CounterState = {
+  count: number;
+};
+
+const initailState: CounterState = {
+  count: 0,
+};
+
+type CounterAction =
+  //ReturnType을 가져오면 함수의 결과물을 가져올 수 있다.
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+function counter(
+  state: CounterState = initailState,
+  action: CounterAction
+): CounterState {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
