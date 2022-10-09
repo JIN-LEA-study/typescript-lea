1f24f89b613e8f206d516cb80ea9b83e7af92801
leaisrevolution <leaisrevolution@gmail.com>
2022-10-10 00:03:27 +0900

    [Add] inference

--- DreamCoding/1-types/1-9-inference.ts
+++ DreamCoding/1-types/1-9-inference.ts
@@ -1,13 +1,23 @@
 /**
  * Type Inference
  */
-let text = 'hello';
-function print(message = 'hello') {
+let text = "hello";
+let text2: string = "hello";
+text = 11; //err
+text = "11";
+
+function print(message = "hello") {
   console.log(message);
 }
-print('hello');
+print("hello");
+print(1); //err
 
 function add(x: number, y: number): number {
   return x + y;
 }
-const result = add(1, 2);
+const result: number = add(1, 2);
+
+/**
+프로젝트에서는 보통 간단하게 코드를 작성하지 않기 때문에
+웬만하면 타입을 명확하게 작성하는 것이 좋다.
+ */
